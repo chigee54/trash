7bd746ded82387e619128aa5ff2af5ed41417f11
chigee54 <86865792+chigee54@users.noreply.github.com>
2022-03-29 22:27:38 +0800

    Update simcse_cosent_dcpcse.py

--- unsupervised sentence embedding/simcse_cosent_dcpcse.py
+++ unsupervised sentence embedding/simcse_cosent_dcpcse.py
@@ -1,9 +1,18 @@
+# Reproduce of simcse, cosent, dcpcse
+# STS results of dcpcse as follows:
+# +-------+-------+-------+-------+-------+--------------+-----------------+-------+
+# | STS12 | STS13 | STS14 | STS15 | STS16 | STSBenchmark | SICKRelatedness |  Avg. |
+# +-------+-------+-------+-------+-------+--------------+-----------------+-------+
+# | 72.36 | 85.29 | 76.47 | 83.76 | 80.19 |    81.05     |      70.09      | 78.46 |
+# +-------+-------+-------+-------+-------+--------------+-----------------+-------+
+
+
 import os
 import argparse
 import time
 import pickle
 import torch
-import torch.nn as nn
+from evaluation_ease import do_senteval
 import torch.nn.functional as F
 import random
 import pandas as pd
@@ -17,6 +26,83 @@ from transformers import BertTokenizer, AdamW, get_linear_schedule_with_warmup
 from Custom_model import Custom_Model, simcse_unsup_loss, cosent_unsup_loss, SCD_Loss
 from scipy.stats import spearmanr
 from torch.utils.tensorboard import SummaryWriter
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-device', default='cuda', type=str)
+    parser.add_argument('-seed', default=42, type=int)
+    parser.add_argument('-pre_seq_len', default=16, type=int)
+    parser.add_argument('-dropout', default=0.1, type=float)
+    parser.add_argument('-dropout_noise', default=0.5, type=float)
+    parser.add_argument('-permutation_num', default=4, type=float)
+    parser.add_argument('-noise_num', default=32, type=float)
+    parser.add_argument('-alpha', default=1.0, type=float)
+    parser.add_argument('-beta', default=0.005225, type=float)
+    parser.add_argument('-lambd', default=0.012, type=float)
+    parser.add_argument('-temp', default=0.05, type=float)
+    parser.add_argument('-embedding_dim', default=768, type=int)
+    parser.add_argument('-projector_num', default="4096-4096-4096", type=str)
+    parser.add_argument('-max_seq_length', default=32, type=int)
+    parser.add_argument('-batch_size', default=256, type=int)
+    parser.add_argument('-eval_batch_size', default=256, type=int)
+    parser.add_argument('-num_epochs', default=1, type=int)
+    parser.add_argument('-learning_rate', default=3e-2, type=float)
+    parser.add_argument('-max_grad_norm', default=1.0, type=float)
+    parser.add_argument('-warm_up_proportion', default=0, type=float)
+    parser.add_argument('-gradient_accumulation_step', default=1, type=int)
+    parser.add_argument('-report_step', default=125, type=int)
+    parser.add_argument('-do_train', default=False, type=bool)
+    parser.add_argument('-do_test', default=True, type=bool)
+    parser.add_argument('-do_senteval', default=True, type=bool)
+    parser.add_argument('-do_ptuning', default=True, type=bool)
+    parser.add_argument('-cls_with_mlp', default=False, type=bool)
+    parser.add_argument('-multi_dropout', default=False, type=bool)
+    parser.add_argument('-overwrite_cache', default=False, type=bool)
+    parser.add_argument('-pool_type', default='cls', type=str)
+    parser.add_argument('-loss', default='CL', choices=['CL', 'CS', 'SCD'], type=str)
+    parser.add_argument('-train_mode', default='unsupervised', choices=['supervised', 'unsupervised'], type=str)
+    parser.add_argument('-output_path', default='output/try3', type=str)
+    parser.add_argument('-bert_path', default='pretrained_model/bert_based_uncased_english', type=str)
+    parser.add_argument('-wiki_file', default='SimCSE-English/data/wiki1m_for_simcse.txt', type=str)
+    parser.add_argument('-nli_file', default='SimCSE-English/data/nli_for_simcse.csv', type=str)
+    parser.add_argument('-train_file', default='SentEval/data/downstream/STS/STSBenchmark/sts-train.csv', type=str)
+    parser.add_argument('-dev_file', default='SentEval/data/downstream/STS/STSBenchmark/sts-dev.csv', type=str)
+    parser.add_argument('-test_file', default='SentEval/data/downstream/STS/STSBenchmark/sts-test.csv', type=str)
+    args = parser.parse_args()
+
+    # config environment
+    os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+    args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    seed_everything(args.seed)
+    args.output_path = join(args.output_path, args.train_mode, 'bsz-{}-lr-{}-dropout-{}'.
+                            format(args.batch_size, args.learning_rate, args.dropout))
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path)
+
+    # model initial
+    tokenizer = BertTokenizer.from_pretrained(args.bert_path)
+    model = Custom_Model(args.bert_path, pooling=args.pool_type, pre_seq_len=args.pre_seq_len,
+                         multi_dropout=args.multi_dropout, dropout_noise=args.dropout_noise,
+                         dropout=args.dropout, do_ptuning=args.do_ptuning)
+    model = torch.nn.DataParallel(model)
+    model.to(args.device)
+
+    # save log and tensorboard
+    cur_time = time.strftime("%Y%m%d_%H_%M", time.localtime())
+    logger.add(join(args.output_path, 'train-{}.log'.format(cur_time)))
+    logger.info(args)
+    writer = SummaryWriter(args.output_path)
+
+    # run
+    start_time = time.time()
+    if args.do_train:
+        train(model, tokenizer, writer, args)
+    if args.do_test:
+        test(model, tokenizer, args)
+    if args.do_senteval:
+        SentEval(model, tokenizer, args)
+    logger.info("run time: {:.2f}".format((time.time() - start_time) / 60))
 
 
 def seed_everything(seed):
@@ -39,9 +125,12 @@ def seed_everything(seed):
 
 class PrepareDataset(Dataset):
     def __init__(self, args, tokenizer, eval_mode=None):
+        self.args = args
         self.overwrite_cache = args.overwrite_cache
         self.wiki_file = args.wiki_file
         self.nli_file = args.nli_file
+        self.dev_file = args.dev_file
+        self.test_file = args.test_file
         self.max_seq_length = args.max_seq_length
         self.tokenizer = tokenizer
         if eval_mode is not None:
@@ -82,7 +171,7 @@ class PrepareDataset(Dataset):
         logger.info('loading supervised train data')
         output_path = os.path.dirname('SimCSE-English/data')
         train_file_cache = join(output_path, 'train-supervised.pkl')
-        if os.path.exists(train_file_cache) and not args.overwrite_cache:
+        if os.path.exists(train_file_cache) and not self.overwrite_cache:
             with open(train_file_cache, 'rb') as f:
                 feature_list = pickle.load(f)
                 logger.info("len of train data:{}".format(len(feature_list)))
@@ -96,7 +185,7 @@ class PrepareDataset(Dataset):
             sent0 = row['sent0']
             sent1 = row['sent1']
             hard_neg = row['hard_neg']
-            feature = tokenizer([sent0, sent1, hard_neg], max_length=self.max_seq_length, truncation=True,
+            feature = self.tokenizer([sent0, sent1, hard_neg], max_length=self.max_seq_length, truncation=True,
                                 padding='max_length', return_tensors='pt')
             feature_list.append(feature)
         with open(train_file_cache, 'wb') as f:
@@ -107,12 +196,12 @@ class PrepareDataset(Dataset):
         logger.info('loading {} data'.format(mode))
         output_path = os.path.dirname('output/unsupervise/')
         eval_file_cache = join(output_path, '{}_32.pkl'.format(mode))
-        if os.path.exists(eval_file_cache) and not args.overwrite_cache:
+        if os.path.exists(eval_file_cache) and not self.overwrite_cache:
             with open(eval_file_cache, 'rb') as f:
                 feature_list = pickle.load(f)
                 logger.info("len of {} data:{}".format(mode, len(feature_list)))
                 return feature_list
-        eval_file = args.dev_file if mode == 'dev' else args.test_file
+        eval_file = self.dev_file if mode == 'dev' else self.test_file
         feature_list = []
         with open(eval_file, 'r', encoding='utf8') as f:
             lines = f.readlines()
@@ -121,9 +210,9 @@ class PrepareDataset(Dataset):
                 line = line.strip().split("\t")
                 assert len(line) == 7 or len(line) == 9
                 score = float(line[4])
-                data1 = tokenizer(line[5].strip(), max_length=args.max_seq_length, truncation=True,
+                data1 = self.tokenizer(line[5].strip(), max_length=self.max_seq_length, truncation=True,
                                   padding='max_length', return_tensors='pt')
-                data2 = tokenizer(line[6].strip(), max_length=args.max_seq_length, truncation=True,
+                data2 = self.tokenizer(line[6].strip(), max_length=self.max_seq_length, truncation=True,
                                   padding='max_length', return_tensors='pt')
                 feature_list.append((data1, data2, score))
         with open(eval_file_cache, 'wb') as f:
@@ -131,17 +220,17 @@ class PrepareDataset(Dataset):
         return feature_list
 
 
-def da_function(y_pred, device, m, n):
+def da_function(y_pred, device, permute_num, noise_num):
     # batch=8时，8对正样本，16条句子，两两对比8*15=120对，故总共有240条句子/batch
     label, da_embedding = [], []
     for i in range(y_pred.shape[0]):
-        for j in range(i + 1, i + m + 1):
+        for j in range(i + 1, i + permute_num + 1):
             if j == y_pred.shape[0]:
                 break
             if i % 3 == 0 and j == i + 1:
                 label.append(2)
             elif i % 3 == 0 and j == i + 2 or i % 2 == 0 and j == i + 1:
-                if str(label).count('1') > n:
+                if str(label).count('1') > noise_num:
                     continue
                 label.append(1)
             else:
@@ -171,7 +260,7 @@ def scheduler_with_optimizer(model, train_loader, args):
     return optimizer, scheduler
 
 
-def evaluate(model, dataloader, cls_with_mlp=False):
+def evaluate(model, dataloader, args):
     model.eval()
     sim_tensor = torch.tensor([], device=args.device)
     label_array = np.array([])
@@ -181,24 +270,23 @@ def evaluate(model, dataloader, cls_with_mlp=False):
             source_input_ids = source.get('input_ids').squeeze(1).to(args.device)
             source_attention_mask = source.get('attention_mask').squeeze(1).to(args.device)
             source_token_type_ids = source.get('token_type_ids').squeeze(1).to(args.device)
-            source_pred = model(source_input_ids, source_attention_mask, source_token_type_ids, cls_with_mlp)
+            source_pred = model(source_input_ids, source_attention_mask, source_token_type_ids, args.cls_with_mlp)
             # target        [batch, 1, seq_len] -> [batch, seq_len]
             target_input_ids = target.get('input_ids').squeeze(1).to(args.device)
             target_attention_mask = target.get('attention_mask').squeeze(1).to(args.device)
             target_token_type_ids = target.get('token_type_ids').squeeze(1).to(args.device)
-            target_pred = model(target_input_ids, target_attention_mask, target_token_type_ids, cls_with_mlp)
+            target_pred = model(target_input_ids, target_attention_mask, target_token_type_ids, args.cls_with_mlp)
             sim = F.cosine_similarity(source_pred, target_pred, dim=-1)
             sim_tensor = torch.cat((sim_tensor, sim), dim=0)
             label_array = np.append(label_array, np.array(label))
     return spearmanr(label_array, sim_tensor.cpu().numpy()).correlation
 
 
-def train(model, args):
+def train(model, tokenizer, writer, args):
     train_dataset = PrepareDataset(args, tokenizer)
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
     dev_dataset = PrepareDataset(args, tokenizer, 'dev')
     dev_loader = DataLoader(dev_dataset, batch_size=args.batch_size, shuffle=True)
-    # 换成普通的优化器有时效果更好：optimizer = torch.optim.AdamW(model.parameters(), lr=args.learning_rate)
     optimizer, scheduler = scheduler_with_optimizer(model, train_loader, args)
     best = 0
     for epoch in range(args.num_epochs):
@@ -225,11 +313,11 @@ def train(model, args):
             optimizer.zero_grad()
             loss.backward()
             step += 1
-            if step % args.gradient_accumulation_step == 0:
-                nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
-                optimizer.step()
-                # scheduler.step()
-                model.zero_grad()
+            # if step % args.gradient_accumulation_step == 0:
+            #     nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+            optimizer.step()
+            # scheduler.step()
+            model.zero_grad()
             if step % args.report_step == 0:
                 corrcoef = evaluate(model, dev_loader, args.do_eval_mlp)
                 logger.info('Epoch[{}/{}], loss:{}, corrcoef: {}'.format
@@ -244,83 +332,19 @@ def train(model, args):
     logger.info('dev_best_corrcoef: {}'.format(best))
 
 
-def test(model, args):
+def test(model, tokenizer, args):
     test_dataset = PrepareDataset(args, tokenizer, 'test')
     test_loader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=True)
     model.load_state_dict(torch.load(join(args.output_path, 'bert.pt')))
-    corrcoef = evaluate(model, test_loader, args.do_eval_mlp)
+    corrcoef = evaluate(model, test_loader, args)
     logger.info('test corrcoef:{}'.format(corrcoef))
 
 
+def SentEval(model, tokenizer, args):
+    model.load_state_dict(torch.load(join(args.output_path, 'bert.pt')))
+    result_STS, result_transfer = do_senteval(tokenizer, model, args.device)
+    logger.info(result_STS)
+
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-device', default='cuda', type=str)
-    parser.add_argument('-seed', default=42, type=int)
-    parser.add_argument('-pre_seq_len', default=16, type=int)
-    parser.add_argument('-dropout', default=0.1, type=float)
-    parser.add_argument('-dropout_noise', default=0.5, type=float)
-    parser.add_argument('-permutation_num', default=4, type=float)
-    parser.add_argument('-noise_num', default=32, type=float)
-    parser.add_argument('-alpha', default=1.0, type=float)
-    parser.add_argument('-beta', default=0.005225, type=float)
-    parser.add_argument('-lambd', default=0.012, type=float)
-    parser.add_argument('-temp', default=0.05, type=float)
-    parser.add_argument('-embedding_dim', default=768, type=int)
-    parser.add_argument('-projector_num', default="4096-4096-4096", type=str)
-    parser.add_argument('-max_seq_length', default=32, type=int)
-    parser.add_argument('-batch_size', default=256, type=int)
-    parser.add_argument('-eval_batch_size', default=256, type=int)
-    parser.add_argument('-num_epochs', default=1, type=int)
-    parser.add_argument('-learning_rate', default=3e-2, type=float)
-    parser.add_argument('-max_grad_norm', default=1.0, type=float)
-    parser.add_argument('-warm_up_proportion', default=0, type=float)
-    parser.add_argument('-gradient_accumulation_step', default=1, type=int)
-    parser.add_argument('-report_step', default=125, type=int)
-    parser.add_argument('-do_train', default=True, type=bool)
-    parser.add_argument('-do_test', default=True, type=bool)
-    parser.add_argument('-do_ptuning', default=True, type=bool)
-    parser.add_argument('-do_eval_mlp', default=False, type=bool)
-    parser.add_argument('-multi_dropout', default=False, type=bool)
-    parser.add_argument('-overwrite_cache', default=False, type=bool)
-    parser.add_argument('-pool_type', default='cls', type=str)
-    parser.add_argument('-loss', default='CL', type=str)
-    parser.add_argument('-train_mode', default='unsupervised', type=str)
-    parser.add_argument('-output_path', default='output/try3', type=str)
-    parser.add_argument('-bert_path', default='pretrained_model/bert_based_uncased_english', type=str)
-    parser.add_argument('-wiki_file', default='SimCSE-English/data/wiki1m_for_simcse.txt', type=str)
-    parser.add_argument('-nli_file', default='SimCSE-English/data/nli_for_simcse.csv', type=str)
-    parser.add_argument('-train_file', default='SentEval/data/downstream/STS/STSBenchmark/sts-train.csv', type=str)
-    parser.add_argument('-dev_file', default='SentEval/data/downstream/STS/STSBenchmark/sts-dev.csv', type=str)
-    parser.add_argument('-test_file', default='SentEval/data/downstream/STS/STSBenchmark/sts-test.csv', type=str)
-    args = parser.parse_args()
-
-    # config environment
-    os.environ['CUDA_VISIBLE_DEVICES'] = '1'
-    args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    seed_everything(args.seed)
-    args.output_path = join(args.output_path, args.train_mode, 'bsz-{}-lr-{}-dropout-{}'.
-                              format(args.batch_size, args.learning_rate, args.dropout))
-    if not os.path.exists(args.output_path):
-        os.makedirs(args.output_path)
-
-    # model initial
-    tokenizer = BertTokenizer.from_pretrained(args.bert_path)
-    model = Custom_Model(args.bert_path, pooling=args.pool_type, pre_seq_len=args.pre_seq_len,
-                         multi_dropout=args.multi_dropout, dropout_noise=args.dropout_noise,
-                         dropout=args.dropout, do_ptuning=args.do_ptuning)
-    model = torch.nn.DataParallel(model)
-    model.to(args.device)
-
-    # save log and tensorboard
-    cur_time = time.strftime("%Y%m%d_%H_%M", time.localtime())
-    logger.add(join(args.output_path, 'train-{}.log'.format(cur_time)))
-    logger.info(args)
-    writer = SummaryWriter(args.output_path)
-
-    # run
-    start_time = time.time()
-    if args.do_train:
-        train(model, args)
-    if args.do_test:
-        test(model, args)
-    logger.info("run time: {:.4f}".format((time.time() - start_time)/60))
+    main()
